ad60e396b41eab54b18961f68d6733c654e5ed14
mbentle7 <mbentle7@emich.edu>
2024-02-12 10:44:09 -0500

    3rd commit

--- src/eclipseSoloW24/HelloWorld.java
+++ src/eclipseSoloW24/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method
 		System.out.println("hello world");
+		System.out.println("hello world 2");
 
 	}
 
