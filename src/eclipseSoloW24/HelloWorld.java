7bbff7457be0130f523ec1725164c756c25c6375
mbentle7 <mbentle7@emich.edu>
2024-02-12 11:06:44 -0500

    commit to cause merge conflict

--- src/eclipseSoloW24/HelloWorld.java
+++ src/eclipseSoloW24/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method
 		System.out.println("hello world");
-		System.out.println("hello world 2 alternate");
+		System.out.println("hello world 2 alternate make changes");
 
 	}
 
